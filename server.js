8a4a5cd34ba8bb3d2aafe123c0acdd33eee24e2d
Aysia Brown <aysiaebrown@gmail.com>
2020-10-29 18:32:56 -0700

    created remove functinoality, updated nav bar

--- server.js
+++ server.js
@@ -34,10 +34,12 @@ function getAllBooks(req, res) {
     .catch(err => console.log(err));
 }
 
+// grabs and renders book details
 app.get('/books/:id', getBookDetails);
 
+// method for grabbing book details from SQL database,
+// and rendering them to the details page
 function getBookDetails(req, res) {
-  console.log('reached!')
   try {
     let query = 'SELECT * FROM books WHERE id=$1';
     let value = [req.params.id];
@@ -54,12 +56,13 @@ function getBookDetails(req, res) {
 }
 
 
-// loads search page
+// renders search page
 app.get('/search', (req, res) => res.render('pages/searches/searches.ejs'));
 
 // loads search results
 app.post('/searches', createSearch);
 
+// makes an api call based on search tick
 function createSearch(req, res) {
   let query = req.body.search[0];
   let url;
@@ -75,20 +78,23 @@ function createSearch(req, res) {
     .catch(err => console.log(err));
 }
 
+// uses form data to add favorited book to database 
 app.post('/books', addToFave);
 
+// method for inserting book into database via SQL
 function addToFave(req,res) {
   const {title, author, isbn, image, description } = req.body;
   let insertBookSQL = `INSERT INTO books (title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) RETURNING id;`;
   client.query(insertBookSQL, [title, author, isbn, image, description])
     .then( data => {
-      console.log("fave", data.rows[0]);
       res.redirect(`/books/${data.rows[0].id}`);
     })
 }
 
+// used for rendering detail edit view
 app.post('/edit', getForm);
 
+// creates form and populates with currently saved values 
 function getForm(req,res) {
   let query = 'SELECT * FROM books WHERE id=$1';
   let { id } = req.body
@@ -99,21 +105,29 @@ function getForm(req,res) {
     })
 }
 
+// used for updating details to database
 app.post('/books/add',editDetails);
 
+// function that handles updating SQL database
 function editDetails(req,res) {
   const {title, author, image, description, isbn, id} = req.body;
   let updateSQL = 'UPDATE books SET title=$1, author=$2, image_url=$3, description=$4, isbn=$5 WHERE id=$6 RETURNING id;';
   client.query(updateSQL, [title, author, image, description, isbn, id])
     .then(data => {
-      console.log("edit", data.rows[0]);
       res.redirect(`/books/${data.rows[0].id}`);
     });
 }
 
+// used for removing book from database
+app.post('/remove', removeFromFave);
 
+// function to perform SQL query to remove selected
+// book from the database 
 function removeFromFave(req,res) {
-  console.log()
+  const { id } = req.body;
+  let deleteSQL = `DELETE FROM books WHERE id = $1;`;
+  client.query(deleteSQL, [id])
+    .then(() => res.redirect('/'));
 }
 
 
